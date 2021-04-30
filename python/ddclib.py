b16917243736287a70b6520ec4321c6a71498450
eleobert <eleobert@hotmail.com>
2021-05-01 02:24:01 +0300

    move functions to ddclib.py file

---
+++ python/ddclib.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import ctypes
+
+lib = ctypes.cdll.LoadLibrary("../cmake-build-debug/libddclib.so")
+
+
+def cor_wrap(x):
+    n = x.shape[1]
+    res = np.zeros((n, n), dtype=np.float64)
+    lib.cor_wrap_c(x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), x.shape[0], x.shape[1],
+                   res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
+    return res
+
+
+# Robust parameter estimator
+def estimate_params(x):
+    n = x.shape[1]
+    locs = np.zeros(n, dtype=np.float64)
+    scales = np.zeros(n, dtype=np.float64)
+    lib.estimate_params_c(x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), x.shape[0], x.shape[1],
+                          locs.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
+                          scales.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
+    return locs, scales
+
+
+def standardize(x):
+    res = np.zeros(x.shape, dtype=np.float64)
+    data = x if type(x) is np.ndarray else x.to_numpy()
+    lib.standardise_c(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), data.shape[0], data.shape[1],
+                   res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
+    if type(x) is pd.DataFrame:
+        res = pd.DataFrame(res, index=x.index, columns=x.columns)
+    return res
+
+
+def predict_univariate(x):
+    res = np.zeros(x.shape, dtype=np.float64)
+    data = x if type(x) is np.ndarray else x.to_numpy()
+    lib.predict_univariate_c(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), data.shape[0], data.shape[1],
+                   res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
+    if type(x) is pd.DataFrame:
+        res = pd.DataFrame(res, index=x.index, columns=x.columns)
+    return res
+
+
+def ddc(x, p = 0.99, min_cor = 0.5):
+    res = np.zeros(x.shape, dtype=np.float64)
+    data = x if type(x) is np.ndarray else x.to_numpy()
+
+    lib.ddc_c(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), data.shape[0], data.shape[1], ctypes.c_double(p), 
+              ctypes.c_double(min_cor), res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
+
+    if type(x) is pd.DataFrame:
+        res = pd.DataFrame(res, index=x.index, columns=x.columns)
+    return res
--- python/test.py
+++ python/test.py
@@ -2,10 +2,8 @@
 
 import pandas as pd
 import numpy as np
-import ctypes
 from scipy import stats
-
-lib = ctypes.cdll.LoadLibrary("../cmake-build-debug/libddclib.so")
+from ddclib import *
 
 df = pd.read_csv("trans_top_gear.csv")
 
@@ -16,56 +14,6 @@ df = df[["Price", "Displacement", "BHP", "Torque", "Acceleration", "TopSpeed", "
          "Height"]]
 df = df.drop(['Citroen C5 Tourer'])
 
-
-def cor_wrap(x):
-    n = x.shape[1]
-    res = np.zeros((n, n), dtype=np.float64)
-    lib.cor_wrap_c(x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), x.shape[0], x.shape[1],
-                   res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
-    return res
-
-
-# Robust parameter estimator
-def estimate_params(x):
-    n = x.shape[1]
-    locs = np.zeros(n, dtype=np.float64)
-    scales = np.zeros(n, dtype=np.float64)
-    lib.estimate_params_c(x.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), x.shape[0], x.shape[1],
-                          locs.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
-                          scales.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
-    return locs, scales
-
-
-def standardize(x):
-    res = np.zeros(x.shape, dtype=np.float64)
-    data = x if type(x) is np.ndarray else x.to_numpy()
-    lib.standardise_c(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), data.shape[0], data.shape[1],
-                   res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
-    if type(x) is pd.DataFrame:
-        res = pd.DataFrame(res, index=x.index, columns=x.columns)
-    return res
-
-
-def predict_univariate(x):
-    res = np.zeros(x.shape, dtype=np.float64)
-    data = x if type(x) is np.ndarray else x.to_numpy()
-    lib.predict_univariate_c(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), data.shape[0], data.shape[1],
-                   res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
-    if type(x) is pd.DataFrame:
-        res = pd.DataFrame(res, index=x.index, columns=x.columns)
-    return res
-
-
-def ddc(x, p = 0.99, min_cor = 0.5):
-    res = np.zeros(x.shape, dtype=np.float64)
-    data = x if type(x) is np.ndarray else x.to_numpy()
-
-    lib.ddc_c(data.ctypes.data_as(ctypes.POINTER(ctypes.c_double)), data.shape[0], data.shape[1], ctypes.c_double(p), 
-              ctypes.c_double(min_cor), res.ctypes.data_as(ctypes.POINTER(ctypes.c_double)))
-
-    if type(x) is pd.DataFrame:
-        res = pd.DataFrame(res, index=x.index, columns=x.columns)
-    return res
 
 sample = ["Audi A4", "BMW i3", "Chevrolet Cruze", "Corvette C6", "Fiat 500 Abarth", "Ford Kuga", "Honda CR-V",
                 "Land Rover Defender", "Mazda CX-5", "Mercedes-Benz G", "Mini Coupe", "Peugeot 107", "Porsche Boxster",
